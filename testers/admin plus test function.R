f3ccb8d1b01497e22b9c34a63680b7cfc7559eef
swanderz <swanson.anders@gmail.com>
2016-05-09 19:39:22 +0800

    cleaning up

--- testers/admin plus test function.R
+++ testers/admin plus test function.R
@@ -18,29 +18,30 @@ xis.db <- read.xlsx2(db.path, sheetIndex = 1, startRow = 4,
 xis.db[ xis.db == "" ] <- NA
 xis.db <- Filter(function(x)!all(is.na(x)), xis.db)
 
-#lubridate and make "Age" and "Years.at.XIS" columns
-xis.db$BIRTH.DATE <- gsub("?","",xis.db$BIRTH.DATE, fixed = TRUE)  %>% dmy()
-xis.db$ENTRY.DAY.1 <- ymd(xis.db$ENTRY.DAY.1, quiet = TRUE)
-
-
-
 
 #take out unneeded columns
-xis.db <- xis.db %>% select(Student.ID = UNIQUE.ID, LAST.NAME, FIRST.NAME,
+xis.db2 <- xis.db %>% select(Student.ID = UNIQUE.ID, LAST.NAME, FIRST.NAME,
                             GRADE.LEVEL, HOMEROOM, HOUSE, GENDER, NATIONALITY,
-                            BIRTH.DATE, Age, ENTRY.DAY.1, Years.at.XIS, X1st.Language,
+                            BIRTH.DATE, ENTRY.DAY.1, X1st.Language,
                             X2nd.Language, Language..home, Mother.speaks,
                             Father.speaks, SCHOOL.BUS, Language.Suppor,
                             Conditional.Pla, Program, LAST.SCHOOL.ATT,Tuition.Paid.by)
 
 #1) filter out PK and K, 2) convert GRADE.LEVEL to numeric,
 #3) FILTER OUT PYP and DP
-xis.db <- xis.db %>% filter(GRADE.LEVEL != "PK" & GRADE.LEVEL != "0K")
-grade <- xis.db$GRADE.LEVEL
-xis.db$GRADE.LEVEL <- suppressWarnings(as.numeric(levels(grade))[grade])
-xis.db <- xis.db %>% filter(GRADE.LEVEL <= 10 & GRADE.LEVEL >= 6)
+xis.db3 <- xis.db2 %>% filter(GRADE.LEVEL != "PK" & GRADE.LEVEL != "0K")
+grade <- xis.db3$GRADE.LEVEL
+xis.db3$GRADE.LEVEL <- suppressWarnings(as.numeric(levels(grade))[grade])
+xis.db3 <- xis.db3 %>% filter(GRADE.LEVEL <= 10 & GRADE.LEVEL >= 6)
+        
+#lubridate and make "Age" and "Years.at.XIS" columns
+xis.db3$BIRTH.DATE <- gsub("?","",xis.db3$BIRTH.DATE, fixed = TRUE)  %>% dmy()
+xis.db3$ENTRY.DAY.1 <- ymd(xis.db3$ENTRY.DAY.1, quiet = TRUE)
+mayday <- ymd(20160506)
+xis.db3$Age <- as.period(interval(start = xis.db3$BIRTH.DATE,
+                                        end = ymd(20160506)))
+               Years.at.XIS = as.period(interval(start = ENTRY.DAY.1,
+                                                 end = ymd(20160506))))
 
+View(xis.db4)
 
-mayday <- ymd("20160506")
-xis.db["Age"] <- as.period(interval(start = xis.db$BIRTH.DATE,end = mayday))
-xis.db["Years.at.XIS"] <- as.period(interval(start = xis.db$ENTRY.DAY.1, end = mayday))
