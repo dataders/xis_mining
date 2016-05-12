a438964c817c03671db8d46fc2a552dadbc513e8
swanderz <swanson.anders@gmail.com>
2016-05-12 17:01:42 +0800

    join 3 dbs

--- concat dbs.R
+++ concat dbs.R
@@ -1,5 +1,4 @@
-
-
+library(ggplot2)
 
 
 setwd("/Users/andersswanson/Desktop/comment\ mining")
@@ -32,12 +31,22 @@ t12.report.byID <- t12.report %>% group_by(Student.ID) %>%
                    avg.CriMean.t2 = mean(CriMean.t2, na.rm = TRUE),
                    growth.avg = mean(class.growth, na.rm = TRUE)) %>%
         within(growth.quartile <- as.integer(cut(growth.avg,
-                                                       quantile(growth.avg, probs=0:4/4,
-                                                                na.rm = TRUE),
-                                                       include.lowest=TRUE)))
+                                                 quantile(growth.avg, probs=0:4/4,
+                                                          na.rm = TRUE),
+                                                 include.lowest=TRUE)))
 
 #joining!
 MB.MAP.db <- inner_join(t12.report.byID, MAP.df, by="Student.ID")
-all <- right_join(xis.db, MB.MAP.db, by="Student.ID")
+all <- right_join(xis.db, MB.MAP.db, by="Student.ID") %>%
+        mutate(
+                Age = as.period(interval(start = BIRTH.DATE,
+                                         end = today())),
+                Years.at.XIS = as.period(interval(start = ENTRY.DAY.1,
+                                                  end = today()))) %>%
+        mutate_each(funs(as.numeric), GRADE.LEVEL) %>%
+        mutate_each(funs(as.numeric), starts_with("Math")) %>%
+        mutate_each(funs(as.numeric), starts_with("Lang")) %>%
+        mutate_each(funs(as.numeric), starts_with("Read")) %>%
+        mutate(career_pct = 100 * Years.at.XIS / (Age - 4))
 
 
