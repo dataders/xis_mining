9c6cda9c19a4a407b8dd1e2821ed8e3c4f095935
swanderz <swanson.anders@gmail.com>
2016-05-16 18:22:30 +0800

    moved lm_eqn to Functions.R

--- Functions.R
+++ Functions.R
@@ -342,3 +342,21 @@ GetMAPbyID <- function(map.path) {
 }
 
 
+# Other -------------------------------------------------------------------
+
+#' plotting library from Stack Overflow
+#' takes a linear model and plots it on the graph w/ R^s values
+lm_eqn = function(m) {
+        l <- list(a = format(coef(m)[1], digits = 2),
+                  b = format(abs(coef(m)[2]), digits = 2),
+                  r2 = format(summary(m)$r.squared, digits = 3));
+        
+        if (coef(m)[2] >= 0)  {
+                eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2,l)
+        } else {
+                eq <- substitute(italic(y) == a - b %.% italic(x)*","~~italic(r)^2~"="~r2,l)
+        }
+        as.character(as.expression(eq));                 
+}
+
+
--- all plotter.R
+++ all plotter.R
@@ -13,31 +13,17 @@ dep <- paste("Math_Percentile")
 
 all.4countries <- all %>% filter(NATIONALITY == "Taiwan" | NATIONALITY == "Korean" | NATIONALITY =="HK" | NATIONALITY == "USA")
 
-thing <- lm(career_pct ~ Read_RITScore, all)
-
-ggplot(all, aes(x=career_pct, y=Read_RITScore)) +
+thing <- lm(Math_RITScore ~ avg.CriMean.t2, all)
+browser()
+ggplot(all, aes(x=Math_RITScore, y=avg.CriMean.t2)) +
         geom_point() +    # Use hollow circles
-        scale_fill_brewer( palette = "Blues") + # Use a slightly darker palette than normal
-        # geom_smooth(method=lm,   # Add linear regression lines
-        #             se=FALSE,    # Don't add shaded confidence region
-        #             fullrange=TRUE) + # Extend regression lines
-        # geom_text(x = 20, y = 260,
-        #           label = lm_eqn(thing), parse = TRUE) +
+        #scale_fill_brewer( palette = "Blues") + # Use a slightly darker palette than normal
+        geom_smooth(method=lm,   # Add linear regression lines
+                    se=FALSE,    # Don't add shaded confidence region
+                    fullrange=TRUE) + # Extend regression lines
+        geom_text(x = 8, y = 6, label = lm_eqn(thing), parse = TRUE)
+
++
         facet_wrap(~GRADE.LEVEL)
 
-ggsave("figures/mat_RITpct_bycountry.png", width = 6, height = 6)
-
-lm_eqn = function(m) {
-        
-        l <- list(a = format(coef(m)[1], digits = 2),
-                  b = format(abs(coef(m)[2]), digits = 2),
-                  r2 = format(summary(m)$r.squared, digits = 3));
-        
-        if (coef(m)[2] >= 0)  {
-                eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2,l)
-        } else {
-                eq <- substitute(italic(y) == a - b %.% italic(x)*","~~italic(r)^2~"="~r2,l)    
-        }
-        
-        as.character(as.expression(eq));                 
-}
+ggsave("figures/mathRIT-T2CriMean-bygrade.png", width = 6, height = 6)
