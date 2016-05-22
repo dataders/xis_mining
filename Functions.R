7809b2c6fec3d80e95d6952d42ac95bd289dca67
swanderz <swanson.anders@gmail.com>
2016-05-22 20:25:27 +0800

    Universalized GetMAPbyID()

--- Functions.R
+++ Functions.R
@@ -346,27 +346,40 @@ GetStudentDBfromAPxlsx <- function(xlsxpath) {
 
 # MAP ---------------------------------------------------------------------
 
+
+#' GetMAPbyID
+#' 
+#' Takes csv of MAP test results (1 obs = 1 test) and:
+#' 1) removes incomplete tests
+#' 2) removes s's 1st attempt if 2nd complete attempt exists
+#' 3) spreads the data so 1 obs = 1 student
 GetMAPbyID <- function(map.path) {
-        map.df <- read.csv(map.path) %>%
-                select(Student.ID = StudentID, Discipline, RITScore= TestRITScore,
-                       StandardError = TestStandardError, Percentile =TestPercentile,
-                       TypicalFallToFallGrowth,
-                       Goal1Name, Goal1RitScore, Goal1StdErr,
-                       Goal2Name, Goal2RitScore, Goal2StdErr,
-                       Goal3Name, Goal3RitScore, Goal3StdErr,
-                       Goal4Name, Goal4RitScore, Goal4StdErr) %>%
-                #convert Student.ID column to factor
-                mutate_each(funs(factor), Student.ID) %>%
-                #hack to take out duplicate tests and "uncombined" test results
-                slice(-c(9,34,473)) %>%
-                slice(-c(365, 366, 519, 520, 637, 638, 655, 656, 904, 905))
+        #test names that don't include all goals
+        discipline.repeats <- c("PRI-READ-Survey w/ Goals CC Intl (Lit/Info,Vocab)V1",
+                                "PRI-READ-Survey w/ Goals CC Intl (FoundSkills,LangWriting)V1",
+                                "PRI-MATH-Survey w/ Goals CC Intl (Meas/Data,Geometry)V1")
         
         #get column of only "TestRIT
-        cols <- colnames(map.df)[3:6]
+        cols <- c("RITScore", "Percentile","Duration.min")
         
-        map.df <- map.df %>% 
-                select(Student.ID:TypicalFallToFallGrowth) %>%
-                unite(temp, RITScore:TypicalFallToFallGrowth, sep = "_") %>%
+        #read in data and named columns below
+        MAP.Raw <- read.csv(map.path) %>%
+                select(Student.ID = StudentID, Discipline, TestName,
+                       RITScore= TestRITScore, Percentile =TestPercentile,
+                       Duration.min = TestDurationInMinutes) 
+        
+        MAP.Clean <- MAP.Raw %>%
+                #remove incomplete tests
+                filter(!(TestName %in% discipline.repeats)) %>%
+                select(everything(), -TestName) %>%
+                arrange(Student.ID, Discipline, desc(RITScore)) %>%
+                mutate(key = paste(Student.ID, Discipline)) %>%
+                distinct(key) %>%
+                select(everything(), -key)
+        
+        
+        MAP.Spread <- MAP.Clean %>% 
+                unite("temp", RITScore:Duration.min, sep = "_") %>%
                 spread(Discipline, temp) %>%
                 separate("Language Usage", paste("Lang", cols, sep = "_"),
                          sep = "_", remove = TRUE, convert = TRUE) %>%
--- load map t2.R
+++ load map t2.R
@@ -1,73 +1,14 @@
 setwd("/Users/andersswanson/Desktop/comment\ mining")
 source("Functions.R")
 
-GetMAPbyID2 <- function(map.path) {
-        map.df <- read.csv(map.path) %>%
-                select(Student.ID = StudentID, Discipline, RITScore= TestRITScore,
-                       StandardError = TestStandardError, Percentile =TestPercentile,
-                       TypicalFallToFallGrowth,
-                       Goal1Name, Goal1RitScore, Goal1StdErr,
-                       Goal2Name, Goal2RitScore, Goal2StdErr,
-                       Goal3Name, Goal3RitScore, Goal3StdErr,
-                       Goal4Name, Goal4RitScore, Goal4StdErr) %>%
-                #convert Student.ID column to factor
-                mutate_each(funs(factor), Student.ID) %>%
-                #hack to take out duplicate tests and "uncombined" test results
-                slice(-c(9,34,473)) %>%
-                slice(-c(365, 366, 519, 520, 637, 638, 655, 656, 904, 905))
-        
-        #get column of only "TestRIT
-        cols <- colnames(map.df)[3:6]
-        
-        map.df <- map.df %>% 
-                select(Student.ID:TypicalFallToFallGrowth) %>%
-                unite(temp, RITScore:TypicalFallToFallGrowth, sep = "_") %>%
-                spread(Discipline, temp) %>%
-                separate("Language Usage", paste("Lang", cols, sep = "_"),
-                         sep = "_", remove = TRUE) %>%
-                separate("Mathematics", paste("Math", cols, sep = "_"),
-                         sep = "_", remove = TRUE) %>%
-                separate("Reading", paste("Read", cols, sep = "_"),
-                         sep = "_", remove = TRUE)
-}
-
-
 MAP.testdate <- c("2015Fall", "2016Spring")
 MAP.path <- paste("data/", MAP.testdate,".Map.Results.csv", sep = "")
 
+
 #Load MAP score database
-MAP.dfs <- lapply(MAP.path, GetMAPbyID)
 
 MAP.og <- read.csv(MAP.path[1])
 
-MAP.df <- GetMAPbyID(MAP.path[1])
-
-#test names that don't include all goals
-discipline.repeats <- c("PRI-READ-Survey w/ Goals CC Intl (Lit/Info,Vocab)V1",
-                        "PRI-READ-Survey w/ Goals CC Intl (FoundSkills,LangWriting)V1",
-                        "PRI-MATH-Survey w/ Goals CC Intl (Meas/Data,Geometry)V1")
-
-Spring.MAP.df <- read.csv(MAP.path[2]) %>%
-        select(Student.ID = StudentID, Discipline, TestName,
-               RITScore= TestRITScore, Percentile =TestPercentile,
-               Duration.min = TestDurationInMinutes) %>%
-        filter(!(TestName %in% discipline.repeats)) %>%
-        mutate(key = paste(Student.ID, Discipline)) %>%
-        slice(-duplicated(key,fromLast=TRUE)) %>%
-        select(everything(), -key, -TestName)
-        
-#get column of only "TestRIT
-cols <- c("RITScore", "Percentile","Duration.min")
-
-
-Spring.MAP.df2 <- Spring.MAP.df %>% 
-        unite("temp", RITScore:Duration.min, sep = "_") %>%
-        spread(Discipline, temp) %>%
-        separate("Language Usage", paste("Lang", cols, sep = "_"),
-                 sep = "_", remove = TRUE, convert = TRUE) %>%
-        separate("Mathematics", paste("Math", cols, sep = "_"),
-                 sep = "_", remove = TRUE, convert = TRUE) %>%
-        separate("Reading", paste("Read", cols, sep = "_"),
-                 sep = "_", remove = TRUE, convert = TRUE)
-
+MAPs <- lapply(MAP.path, GetMAPbyID)
+Spring.MAP.df <- GetMAPbyID(MAP.path[2])
 
