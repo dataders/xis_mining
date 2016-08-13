52ecf1bd04ed2d0ad77f000485812f2c394d8f81
swanderz <swanson.anders@gmail.com>
2016-08-13 18:39:08 -0400

    clearer structure

--- all pruned tfidf lists.R
+++ all pruned tfidf lists.R
@@ -7,16 +7,26 @@ t1.report <- GetReportsDFfromMBcsv("data/t1 comments.csv")
 t2.report <- GetReportsDFfromMBcsv("data/t2 comments.csv")
 t3.report <- GetReportsDFfromMBcsv("data/t3 comments.csv")
 
+#make a corpus of all the reports for the year
 year.corpus <- c(GetCorpusFromReportDF(t1.report), GetCorpusFromReportDF(t2.report),GetCorpusFromReportDF(t3.report))
 
-#teachers
+
+# Teachers ----------------------------------------------------------------
+
+#get a corpus where each doc is all words written by one teacher
 teacher.corpus <- GetGroupCorpusfromCommentCorpus(year.corpus, "teacher")
+
+#make an ngram Tf-Idf matrix for each teacher where 2≤n≤7
 system.time(all.tfidf <- GetAllTfIdfMatricesFromGroupedCorpus(teacher.corpus, 2, 7, TRUE))
 
 
-#students
+
+# Students ----------------------------------------------------------------
+
+#get a corpus where each doc is all words written about one student
 student.corpus <- GetGroupCorpusfromCommentCorpus(year.corpus, "student")
 
+#make an ngram Tf-Idf matrix for each student where 2≤n≤7
 system.time(
         all.pruned <- GetMemberPrunedList(student.corpus, 2, 7, TRUE, 200)
 )
