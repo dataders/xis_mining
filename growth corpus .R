f65b711dbe1fedcd68dd5a8f385c288e56715a57
swanderz <swanson.anders@gmail.com>
2017-01-15 13:53:22 -0500

    remove dependency & z-score calculation

--- growth corpus .R
+++ growth corpus .R
@@ -18,55 +18,63 @@
 #set wd and paths and source functions
 setwd("/Users/andersswanson/Desktop/comment\ mining")
 source("Functions.R")
-source("MB - t12 growth.R")
+#source("MB - t12 growth.R")
 
 
 # B) t1 Comments & Corpus ---------------------------------------
 
-t1.report <- GetReportsDFfromMBcsv("data/t1 comments.csv") %>% AnonymizeReport()
+t1.report <- GetReportsDFfromMBcsv("data/t1 comments.csv") %>% 
+        AnonymizeReport()
 t1.corpus <- GetCorpusFromReportDF(t1.report)
 t1.corpus.student <- GetGroupCorpusfromCommentCorpus(t1.corpus, "student")
 
 
-# C) Year Report w/ z-score ------------------------------------------
+# C) Year Report w/ centered mean ------------------------------------------
 
-# get year report with added z-scored t12.growth score appended
-year.report.byclass <- GetT12StdGrowthfromYearReport() %>%
-        #make quintiles of t12.zgrowth variable
-        within(t12.zgrowth.quintile <- as.integer(cut(t12.zgrowth,
-                                                       quantile(t12.zgrowth, probs=0:5/5,
-                                                                na.rm = TRUE),
-                                                       include.lowest=TRUE))) %>%
-        #add ID.SUB column (for cross-ref w/ t1 corpus)
+
+#wrappers for mean and sd with na.rm = TRUE
+av <- function(x) {mean(x, na.rm = TRUE)}
+s <- function(x) {sd(x, na.rm = TRUE)}
+
+year.report <- GetYearReport() %>%
+        select(Student.ID, Class.ID:Teacher, CriMean.t1, CriMean.t2, t12.growth) %>%
+        group_by(Class.ID) %>%
+        mutate(t12.growth.center = round(t12.growth - av(t12.growth), 22)) %>%
+        ungroup() %>%
+        within(t12.growth.center.quartile <- as.integer(cut(t12.growth.center,
+                                                      quantile(t12.growth.center, probs=0:4/4,
+                                                               na.rm = TRUE),
+                                                      include.lowest=TRUE))) %>%
         mutate(ID.SUB = paste(Student.ID, Subject))
 
 
-# D) quintile Corpus ------------------------------------------------------------
+# D) quartile Corpus ------------------------------------------------------------
 
-#get 4 quintiles of ID.SUB's 
-quints <- c(1,2,3,4,5)
-quintiles <- lapply(quints, function(x) {
-        year.report.byclass %>% filter(t12.zgrowth.quintile == x) %>%
+#get 4 quartiles of ID.SUB's 
+quarts <- c(1,2,3,4)
+quartiles <- lapply(quarts, function(x) {
+        year.report %>% filter(t12.growth.center.quartile == x) %>%
                 .$ID.SUB
 })
 
-#paste each quintile's comments into one comment
-quintile.comments <- lapply(quintiles, function(x) {
+#paste each quartile's comments into one comment
+quartile.comments <- lapply(quartiles, function(x) {
         idx <- t1.corpus %>% meta(tag = "ID.SUB") %in% x
         do.call(paste,content(t1.corpus[idx]))
 })
 
+top.bottom.comments <- quartile.comments[-c(2,3)]
 
-#make corpus (1 quintile = 1 document)
-quintile.corpus <- VectorSource(quintile.comments) %>% Corpus
+#make corpus (1 quartile = 1 document)
+quartile.corpus <- VectorSource(quartile.comments) %>% Corpus
 
 
 # E) Analysis -------------------------------------------------------------
 
 #make dtc from corpus
-all.tfidf <- GetAllTfIdfMatricesFromCorpus(quintile.corpus, 1,3, norm = TRUE)
+all.tfidf <- GetAllTfIdfMatricesFromCorpus(quartile.corpus, 1,3, norm = TRUE)
 
-all.pruned <- lapply(all.tfidf, GetPrunedList, prune_thru = 200)
+all.pruned <- lapply(all.tfidf, GetPrunedList, prune_thru = 100)
 
 
 test <- cbind(unlist(sapply(1:5, function(x) {all.pruned[[x]]}), recursive = FALSE))
